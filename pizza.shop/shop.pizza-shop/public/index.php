83688722b95f02c0b7039ff3acff22baeb468114
lucarafe <luc.bonin0903@gmail.com>
2023-09-12 17:00:25 +0200

    ajout ServiceCommande.php

--- pizza.shop/shop.pizza-shop/public/index.php
+++ pizza.shop/shop.pizza-shop/public/index.php
@@ -7,5 +7,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 /* application boostrap */
 $appli = require_once __DIR__ . '/../config/bootstrap.php';
 
-
+$serv = new \pizzashop\shop\domain\services\commande\ServiceCommande();
+$serv->accederCommande('f669ac6c-8ea5-3800-a61d-7dfef86ba2d9');
 $appli->run();
--- pizza.shop/shop.pizza-shop/src/domain/services/commande/ServiceCommande.php
+++ pizza.shop/shop.pizza-shop/src/domain/services/commande/ServiceCommande.php
@@ -1,5 +1,47 @@
 <?php
 namespace pizzashop\shop\domain\services\commande;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use pizzashop\shop\domain\dto\commande\commandeDTO;
+use pizzashop\shop\domain\dto\commande\itemDTO;
+use pizzashop\shop\domain\entities\commande\Commande;
+
 class ServiceCommande {
+    /**
+     * @throws ServiceCommandeNotFoundException
+     */
+    public function accederCommande($UUID)
+    {
+        try {
+            $commande = Commande::findOrFail($UUID);
+
+            // Créer un objet CommandeDTO
+            $commandeDTO = new CommandeDTO();
+            $commandeDTO->id = $commande->id;
+            $commandeDTO->date_commande = $commande->date_commande;
+            $commandeDTO->type_livraison = $commande->type_livraison;
+            $commandeDTO->montant_total = $commande->montant_total;
+            $commandeDTO->delai = $commande->delai;
+            $commandeDTO->email_client = $commande->client->email; // Supposons que la relation client soit définie
+
+            // Créer une liste d'ItemDTO
+            $itemDTOs = [];
+            foreach ($commande->items as $item) {
+                $itemDTO = new ItemDTO();
+                $itemDTO->numero = $item->numero;
+                $itemDTO->libelle = $item->libelle;
+                $itemDTO->taille = $item->taille;
+                $itemDTO->tarif = $item->tarif;
+                $itemDTO->quantite = $item->quantite;
+                $itemDTOs[] = $itemDTO;
+            }
+
+            $commandeDTO->items = $itemDTOs;
+            echo $commandeDTO;
+            return $commandeDTO;
+        } catch (ModelNotFoundException $e) {
+            // Gérer le cas où la commande avec l'ID donné n'a pas été trouvée.
+            return "Commande non trouvée.";
+        }
+    }
 
 }
