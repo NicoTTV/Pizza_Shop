e972d7e4d3474590c6818515d145d03e82d70e53
hergott18u <emilien.herg@gmail.com>
2023-09-12 17:26:11 +0200

    création fichier de logs

--- pizza.shop/shop.pizza-shop/src/domain/services/commande/CommandeLogger.php
+++ pizza.shop/shop.pizza-shop/src/domain/services/commande/CommandeLogger.php
@@ -3,10 +3,11 @@
 namespace pizzashop\src\domain\services\commande;
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Level;
 use Monolog\Logger;
 
 $logger = new Logger('logger');
-$logger->pushHandler(new StreamHandler('', Logger::WARNING));
+$logger->pushHandler(new StreamHandler(__DIR__ . '/../../../logs', Level::Warning));
 
 $logger->debug('debug');
 $logger->warning('warning');
