270a2ba076dc76a9d3ac297cd41cc045810c7f56
lucarafe <luc.bonin0903@gmail.com>
2023-09-19 15:29:28 +0200

    modif classes

--- pizza.shop/shop.pizza-shop/src/domain/entities/commande/Item.php
+++ pizza.shop/shop.pizza-shop/src/domain/entities/commande/Item.php
@@ -2,6 +2,8 @@
 
 namespace pizzashop\shop\domain\entities\commande;
 
+use pizzashop\shop\domain\dto\commande\ItemDTO;
+
 class Item extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'item';
@@ -11,5 +13,13 @@ class Item extends \Illuminate\Database\Eloquent\Model
     public function commande()
     {
         return $this->belongsTo(Commande::class, 'commande_id');
+    }
+
+    function toDTO(){
+        $itemDTO = new ItemDTO($this->numero, $this->taille, $this->quantite);
+        $itemDTO->setLibelle($this->libelle);
+        $itemDTO->setTarif($this->tarif);
+        $itemDTO->setLibelleTaille($this->libelle_taille);
+        return $itemDTO;
     }
 }
