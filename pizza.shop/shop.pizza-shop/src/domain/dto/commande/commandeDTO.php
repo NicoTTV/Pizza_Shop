36ae9d7e4d6ba7a0e9d21599757fbd2e58440d97
lucarafe <luc.bonin0903@gmail.com>
2023-09-19 14:32:57 +0200

    modif constructeur commandeDTO.php

--- pizza.shop/shop.pizza-shop/src/domain/dto/commande/commandeDTO.php
+++ pizza.shop/shop.pizza-shop/src/domain/dto/commande/commandeDTO.php
@@ -15,30 +15,18 @@ class commandeDTO extends \pizzashop\shop\domain\dto\DTO{
     private String $email_client;
     private array $items;
 
-    public function __construct(String $id, String $date_commande, Int $type_livraison, float $montant, Int $delai, String $email_client, array $items)
-    {
+    public function __construct(String $email_client, Int $type_livraison) {
 
-        $this->id = $id;
-        $this->date_commande = $date_commande;
         $this->type_livraison = $type_livraison;
-        $this->montant = $montant;
-        $this->delai = $delai;
         $this->email_client = $email_client;
-        $this->items = $items;
     }
 
-    /**
-     * @throws PropertyDoesNotExist
-     */
     public function __get($att){
         if (property_exists($this, $att))
             return $this->$att;
         else throw new PropertyDoesNotExist("Property $att does not exist");
     }
 
-    /**
-     * @throws PropertyDoesNotExist
-     */
     public function __set(string $name, $value): void
     {
         if (property_exists($this, $name))
