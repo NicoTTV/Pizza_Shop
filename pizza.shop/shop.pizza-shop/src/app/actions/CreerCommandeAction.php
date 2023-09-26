a0f78a3f8f77360c2a0cb4ffe98503f1a5fa3084
lucarafe <luc.bonin0903@gmail.com>
2023-09-26 15:39:57 +0200

    correction ItemDTO

---
+++ pizza.shop/shop.pizza-shop/src/app/actions/CreerCommandeAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pizzashop\shop\app\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CreerCommandeAction {
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface {
+
+    }
+
+}
