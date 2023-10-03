60fbf722f305df13ae8194b604ad4766d45852f5
lucarafe <luc.bonin0903@gmail.com>
2023-10-03 17:07:45 +0200

    FIN TD3

--- pizza.shop/shop.pizza-shop/config/services.php
+++ pizza.shop/shop.pizza-shop/config/services.php
@@ -6,8 +6,4 @@ return [
     'catalogue.service' => function(\Psr\Container\ContainerInterface $container){
         return new \pizzashop\shop\domain\services\catalogue\ServiceCatalogue();
     },
-    \pizzashop\shop\app\actions\AccederCommandeAction::class => function(\Psr\Container\ContainerInterface $container){
-        return new \pizzashop\shop\app\actions\AccederCommandeAction($container->get('commande.service'));
-    },
-
 ];
