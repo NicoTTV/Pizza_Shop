a2a09de0d2ae257a6a110ca741bbb20c3c876d0a
Nicolas Kuenemann <kuenemannnicolas@gmail.com>
2023-10-03 15:49:23 +0200

    Ajout fichier sql authentification et docker compose

---
+++ pizza.shop/auth.pizza-shop/sql/pizza_shop.auth.data.sql
@@ -0,0 +1,51 @@
+-- Adminer 4.8.1 MySQL 5.5.5-10.3.11-MariaDB-1:10.3.11+maria~bionic dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+INSERT INTO `users` (`email`, `password`, `active`, `activation_token`, `activation_token_expiration_date`, `refresh_token`, `refresh_token_expiration_date`, `reset_passwd_token`, `reset_passwd_token_expiration_date`) VALUES
+('AlixPerrot@free.fr',	'$2y$10$i51WT9RDQjBZHtExysLfq.RKxZ9FW1ftto.EEuuWt76pxyTGYi32C',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('AlphonseFleury@sfr.fr',	'$2y$10$t8uoMqP1/QY1i7WgCJqYd.PC2MXKXxL9gqpTJTblbqxKCswQiq6F2',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('ArnaudeLeblanc@hotmail.fr',	'$2y$10$Opc9TIPsWihJ0IdxNMUh7uzBu.qQOmI7K/XP1P3/BpBws5Ti6WaxC',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('BertrandMallet@orange.fr',	'$2y$10$3JMcjBY8zY.maLgW2wtLHe/GQIVs9gqXHdzng3q3TpLVCpxr20Ek.',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('BrigitteRenard@sfr.fr',	'$2y$10$SzXQIhoSf84fJkXvm45o2OLnP/IMgF2N5nwLRH7lOHHRDgTijI2Uy',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('CélinaDeschamps@club-internet.fr',	'$2y$10$Lw9E4xZ8Bihj9cAjcOVtneeS1ZPKN9bgvR.N5v5iil4votdsVvYFu',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('EmmanuelBlin@dbmail.com',	'$2y$10$JXyfOjLv//UgeF9fID/UcOBR72LQ.r7xY2fwR2KgeIyeWXNgAtr5G',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('ÉtienneGuyon@gmail.com',	'$2y$10$UvQL/1sHwvquS7pATY8Gtuoy66sMs3HFs/cBO1QvT9L/MV6pLjYhO',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('EugèneClement@sfr.fr',	'$2y$10$ucd5nX4XwLX48Szh/TGHoOaSmScJq6HFkscH4KtB.BfpBMV7LkZMy',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('FranckRemy@noos.fr',	'$2y$10$l7CDMDuf/et.UgyAgUhKYuvKXXIym1BamGrphZRQ2uNP3KhsGqXqC',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('FrançoiseFischer@sfr.fr',	'$2y$10$E5uUArnSsgWCJjHtwRktMeOkWvuQQ77qlHAP5jT2ElmMMfkk2Y85S',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('GeorgesGuichard@laposte.net',	'$2y$10$iwNjaxjtBtFUWR2kuMBdDOy0/S9l4Ori7cbEkpcRUf4MdCLxn/.jO',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('GérardMartins@sfr.fr',	'$2y$10$lL0CmqytyvL8MmJOavNWZ.Ims./8pOgY6YhpJXQMteepr5z80BD6i',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('GrégoireDupont@tele2.fr',	'$2y$10$Xj3mb2hO/.Nw2MiinA/zEO34MrYsN1E3JnjbJ3og/Oofkc1Npfc2O',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('GuillaumePerret@live.com',	'$2y$10$I.oWADIxQHW/.EQkjAftTe4fzJWKn.VnrntQwfBFKev5/6vej70B.',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('GuyBoutin@sfr.fr',	'$2y$10$90ppPt7JpT.iyuLBI7mtLOtr0MOaXqJRai7vrIBt38acdzqxi72uG',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('HélèneCoulon@orange.fr',	'$2y$10$VZZOpJ4mtE..fWVjm2aPdu034a5rHVL5VTzHHOnZHuRMX/aJKYnsa',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('HélèneGuillaume@hotmail.fr',	'$2y$10$AODYB.zWbqHuyoJuONJtsedgUZ81J.LHNcFzz/q.Ef2T8u6X4Qz1a',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('LaurenceLambert@live.com',	'$2y$10$kSIOvw3ymvtK9at9FG.l2OVvrD1/n8WTtx4QP5HqQnUdw3uheVf2S',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('LucasBailly@yahoo.fr',	'$2y$10$nCtaxRbhuAZvzxBDFx43cedpOsWxN5a9Lk.Thm1F9DP2gbxdvNOk6',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('LuceNicolas@club-internet.fr',	'$2y$10$p37bNCIPD.v.J3.inkG3peCwR05fgr2eMSKntbW13byqKtbJx38/S',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('LucyColas@yahoo.fr',	'$2y$10$NUNEX78a4/Pq1jC/sG5K4.5rHGrRS5mQNF3Jypg54756LPCsgbdjO',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('MarcMarin@gmail.com',	'$2y$10$de4KVwXKoiAuFk4m9ZyjO.O4pCq4trOYmBumE9qBGDGA1X9JDibii',	0,	NULL,	NULL,	'453a2bbf4246c53140dda3b4b5271e8e79f8dd03e32243ac5f7fd91cffac7832',	'2023-09-23 15:25:33',	NULL,	NULL),
+('MargaudGodard@free.fr',	'$2y$10$uUjBqHN6W4MjRK7TPgZCAOSWpMTqnCyxJ69IBo42562V1xv3CaEmm',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('MathildePayet@orange.fr',	'$2y$10$6QZbQ2vjRH6IPxqar2TcQOHJbj8wXFBOSkvsoolCftU3L5JQUK9m6',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('MauriceRobin@yahoo.fr',	'$2y$10$uCra5Zn4EuprSKmBYQ5g/eW6aR.yKz/yhDWt1Q4nVYVuIiO3ZqXDC',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('miche@gmal.com',	'$2y$10$8zkitJOs18Y0UYAGYRDSpOWAFIR2sHbgJ7V6Hz92VKzAMHi9eZaru',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('OcéaneBonnin@live.com',	'$2y$10$G57o5XlkDGOrUdaV1d7Go.w4b8ZbwTtHmRcRVJKu.5AJmOMJcmShm',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('OlivieGillet@club-internet.fr',	'$2y$10$gZfrsoL813w9XsQvMfYafeGwArcm4TGTOE8B9VJc.z3dIPj31LyPa',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('PaulRemy@noos.fr',	'$2y$10$sFtCEmM.3O5A4IaDmTK1g.zkjGB/LQNRX.t1EFa05THniicKkUykK',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('RenéePeron@yahoo.fr',	'$2y$10$jrUKXmO1iTT60yDEbqOL1e/CudEuveHp0UHh1GrWug8leP8bVEAM2',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('RobertRey@dbmail.com',	'$2y$10$W0ucVMttcVV7uEpz08YOxOKEje7vWaLdJNFKovoNUUISYwzpBwbG2',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('RogerCarlier@wanadoo.fr',	'$2y$10$e6jHDBhdDLi6WfceLOpOwu3I1vwMjvwHWCAoxmKKVQJvfPu8L2tGm',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('RolandSchmitt@club-internet.fr',	'$2y$10$y5G7pD2RmfQghMrkujf.F.vG8gDmvfP4FynkU2qPCCV2PHai4OYaW',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('StéphaneLouis@club-internet.fr',	'$2y$10$o102IHg1NdshdqiqYuVHwOosCWgn3vEIZ0Z73rgTuiP1LfRP/Dgn6',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('SusanneCouturier@tele2.fr',	'$2y$10$IcTIPuTtu4E.U29rlcHYRuD8BaB.VgWpb8NEASNlWt9tfg6Xs/vkW',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('ThéodoreLeger@sfr.fr',	'$2y$10$vN7ap.w1fvcH1KDKsaAeO.Wi2AZOGyb/WUSiGOoSlQKHkPpwP3EEe',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('ThéophileBouvier@live.com',	'$2y$10$9YRWPGJHbdWKL0nI6JTpUuv5hpLIxzHGwkCWuCNYERdTROwIbDa0u',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('ThomasBaudry@dbmail.com',	'$2y$10$qWAhCWU0XhPFYfcVI/3SB.MW6LVWUDCL/GRvIV194w7axbJW7l/..',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('VincentLaine@tele2.fr',	'$2y$10$h3HzA2hHOs0cx/ZSxQhUk.a1uavTenWAa6d4WOR.CQQL7kbwiE0J.',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+('ZacharieLefort@sfr.fr',	'$2y$10$vJp/RhxpjiD/dhrfvNfm7.LTahaAV9bgx6b2ZaybASZFSVydExkOm',	0,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL);
+
+-- 2023-09-25 08:37:32
