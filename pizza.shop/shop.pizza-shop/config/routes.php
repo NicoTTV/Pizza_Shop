ea6b24760c71c64b65ff1b3217979e967360f688
lucarafe <luc.bonin0903@gmail.com>
2024-01-08 22:24:58 +0100

    rajout cors perdu

--- pizza.shop/shop.pizza-shop/config/routes.php
+++ pizza.shop/shop.pizza-shop/config/routes.php
@@ -29,21 +29,6 @@ return function( \Slim\App $app):void {
     $app->get('/categories/{id_categorie}/produits[/]', \pizzashop\shop\app\actions\ListerProduitsParCategorieAction::class)
         ->setName('produits_par_categorie');
 
-    $app->get('/produits[/]', \pizzashop\shop\app\actions\ListerProduitsAction::class)
-        ->setName('produits');
-
-    $app->get('/produits/{id_produit}[/]', \pizzashop\shop\app\actions\AccederProduitAction::class)
-        ->setName('produit');
-
-    $app->get('/categories/{id_categorie}/produits[/]', \pizzashop\shop\app\actions\ListerProduitsParCategorieAction::class)
-        ->setName('produits_par_categorie');
-
-
-    $app->get('/test', function (Request $request, Response $response) {
-        $response->getBody()->write("Hello World");
-        return $response->withHeader('Content-Type', 'text/plain');
-    });
-
     $app->options('/{routes:.+}', function ($request, $response, $args) {
         return $response;
     });
