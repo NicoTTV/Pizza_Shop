3e9cc29d43148da05a5ac470056cc5bcfabc1f7e
lucarafe <luc.bonin0903@gmail.com>
2024-01-09 17:28:39 +0100

    modification des varialbe JWT en variable d'enviro

--- pizza.shop/auth.pizza-shop/config/settings.php
+++ pizza.shop/auth.pizza-shop/config/settings.php
@@ -3,6 +3,6 @@ return [
     'displayErrorDetails' => true,
     'auth.db.config' => __DIR__ . '/auth.db.ini',
     'auth.db.config.name' => 'auth',
-    'JWT_SECRET' => 'f03293f7a1a9f1ed321c3079903430f2462ca070bb55a0d78dbd9eda77049a66',
-    'JWT_EXPIRES_IN_S' => 3600
+    'JWT_SECRET' => getenv('JWT_SECRET'),
+    'JWT_EXPIRES_IN_S' => getenv('JWT_EXPIRES_IN_S'),
 ];
