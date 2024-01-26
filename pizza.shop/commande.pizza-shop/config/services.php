9b259dace44661689e7792763a0b2c8e1363b047
lucarafe <luc.bonin0903@gmail.com>
2024-01-26 16:53:08 +0100

    TD 8 avant test

--- pizza.shop/commande.pizza-shop/config/services.php
+++ pizza.shop/commande.pizza-shop/config/services.php
@@ -1,12 +1,18 @@
 <?php
 return [
     'commande.service' => function(\Psr\Container\ContainerInterface $container) {
-        return new \pizzashop\commande\domain\services\commande\ServiceCommande($container->get('catalogue.service'));
+        return new \pizzashop\commande\domain\services\commande\ServiceCommande($container->get('catalogue.service'), $container->get('rabbitmq.channel'));
     },
     'catalogue.service' => function(\Psr\Container\ContainerInterface $container) {
         return new \pizzashop\commande\domain\services\commande\ServiceCatalogue();
     },
     'middleware.checkIfOwner' => function(\Psr\Container\ContainerInterface $container){
         return new \pizzashop\commande\app\middleWare\CheckIfOwner($container->get('commande.service'));
+    },
+    'rabbitmq.channel' => function(\Psr\Container\ContainerInterface $container){
+        return $container->get('rabbitmq.connection')->channel();
+    },
+    'rabbitmq.connection' => function(\Psr\Container\ContainerInterface $container){
+        return new \PhpAmqpLib\Connection\AMQPStreamConnection($container->get('rabbitmq.host'), $container->get('rabbitmq.port'), $container->get('rabbitmq.user'), $container->get('rabbitmq.password'));
     },
 ];
--- pizza.shop/commande.pizza-shop/config/settings.php
+++ pizza.shop/commande.pizza-shop/config/settings.php
@@ -3,5 +3,8 @@ return [
     'displayErrorDetails' => true,
     'com.db.config' => __DIR__ . '/commande.db.ini',
     'com.db.config.name' => 'commande',
-
+    'rabbitmq.host' => 'localhost',
+    'rabbitmq.port' => 4500,
+    'rabbitmq.user' => 'commande',
+    'rabbitmq.password' => 'commande',
 ];
